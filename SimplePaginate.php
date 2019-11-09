624da37c9308e5d3c23d4eb1743e6bb3b1e1f29d
nickrod <nickrod@users.noreply.github.com>
2019-11-08 22:09:28 -0800

    Add validation

--- SimplePaginate.php
+++ SimplePaginate.php
@@ -26,7 +26,7 @@ class SimplePaginate
 
   // page links offset
 
-  private $page_links_offset;
+  private $page_links_offset = 3;
 
   // url params
 
@@ -65,26 +65,59 @@ class SimplePaginate
   //public function __construct($total_records, $per_page, $current_page, $canonical_url, $total_page_links = 6, $url_params = '')
   public function __construct($options = [])
   {
-    $this->total_records = $total_records;
-    $this->per_page = $per_page;
-    $this->canonical_url = addslashes($canonical_url);
-    $this->page_links_offset = $page_links_offset;
-    $this->url_params = addslashes($url_params);
+    if (!empty($options['total_records']) && is_int($options['total_records']))
+    {
+      $this->total_records = $options['total_records'];
+    }
+
+    //
+
+    if (!empty($options['per_page']) && is_int($options['per_page']))
+    {
+      $this->per_page = $options['per_page'];
+    }
+
+    //
+
+    if (!empty($options['canonical_url']) && is_string($options['canonical_url']) && filter_var($options['canonical_url'], FILTER_VALIDATE_URL))
+    {
+      $this->canonical_url = addslashes($options['canonical_url']);
+    }
+
+    //
+
+    if (!empty($options['page_links_offset']) && is_int($options['page_links_offset']))
+    {
+      $this->page_links_offset = $options['page_links_offset'];
+    }
+
+    //
+
+    if (!empty($options['url_params']) && is_string($options['url_params']))
+    {
+      $this->url_params = filter_var($options['url_params'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    // calculate total pages
+
     $this->total_pages = ceil($this->total_records / $this->per_page);
 
     // set current page, do some basic validation
 
-    if ($current_page < 1)
+    if (!empty($options['current_page']) && is_int($options['current_page']))
     {
-      $this->current_page = 1;
-    }
-    elseif ($current_page > $this->total_pages && $this->total_pages > 0)
-    {
-      $this->current_page = $this->total_pages;
-    }
-    else
-    {
-      $this->current_page = $current_page;
+      if ($options['current_page'] < 1)
+      {
+        $this->current_page = 1;
+      }
+      elseif ($options['current_page'] > $this->total_pages && $this->total_pages > 0)
+      {
+        $this->current_page = $this->total_pages;
+      }
+      else
+      {
+        $this->current_page = $options['current_page'];
+      }
     }
 
     // set previous and next pages
@@ -166,7 +199,7 @@ class SimplePaginate
   //public function getLinks($ul_class = "", $li_class = "", $a_class = "")
   public function getLinks($options = [])
   {
-    $tags = '';
+    $links = '';
 
     // only show paging links if there is more than one page
 
@@ -178,14 +211,14 @@ class SimplePaginate
 
       //
 
-      $tags .= "<ul $ul_class>";
+      $links .= "<ul $ul_class>";
 
       // show prev/first links
 
       if ($this->current_page > 1)
       {
-        $tags .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $this->previous_page . $this->url_params . "'>Previous</a></li>";
-        $tags .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=1" . $this->url_params . "'>First</a></li>";
+        $links .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $this->previous_page . $this->url_params . "'>Previous</a></li>";
+        $links .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=1" . $this->url_params . "'>First</a></li>";
       }
 
       // page links
@@ -195,11 +228,11 @@ class SimplePaginate
         if ($i == $this->current_page)
         {
           $li_class = (!empty($li_class)) ? ("class='" . $li_class . " active'") : "class='active'";
-          $tags .= "<li $li_class><b>$i</b></li>";
+          $links .= "<li $li_class><b>$i</b></li>";
         }
         else
         {
-          $tags .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $i . $this->url_params . "'>" . $i . "</a></li>";
+          $links .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $i . $this->url_params . "'>" . $i . "</a></li>";
         }
       }
 
@@ -207,17 +240,17 @@ class SimplePaginate
 
       if ($this->current_page < $this->total_pages)
       {
-        $tags .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $this->total_pages . $this->url_params . "'>Last</a></li>";
-        $tags .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $this->next_page . $this->url_params . "'>Next</a></li>";
+        $links .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $this->total_pages . $this->url_params . "'>Last</a></li>";
+        $links .= "<li $li_class><a $a_class href='" . $this->canonical_url . "?page=" . $this->next_page . $this->url_params . "'>Next</a></li>";
       }
 
       //
 
-      $tags .= "</ul>";
+      $links .= "</ul>";
     }
 
     //
 
-    return $tags;
+    return $links;
   }
 }
