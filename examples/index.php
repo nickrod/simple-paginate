d094a0acddf018a4e11b92b9d028c68a99bbb24c
nickrod <nickrod@users.noreply.github.com>
2019-11-17 12:36:49 -0800

    Rename examples to index

--- examples/index.php
+++ examples/index.php
@@ -10,5 +10,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 //
 
-$page = new SimplePaginate();
+$page = new SimplePaginate(['current_page' => 2]);
+$page->setCurrentPage(4);
 echo $page->getLinks();
